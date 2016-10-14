d3c98ff1809d1c761600ffaf7f643ec8facc2d6f
Jakub Gruszczyński <Jakub Gruszczyski>
2016-10-14 16:25:34 +0200

    Fixes in local search algorithm

--- src/main/java/HalfTSPLocalSearch.java
+++ src/main/java/HalfTSPLocalSearch.java
@@ -17,6 +17,7 @@ public class HalfTSPLocalSearch {
         List<Integer> freeVertices = IntStream.range(0, graph.getVerticesCount()).filter(v -> !cycle.contains(v)).boxed().collect(Collectors.toList());
         int verticesInCycleCount = graph.getVerticesCount() / 2;
         int[][] adjacencyMatrix = graph.getAdjacencyMatrix();
+
         do {
             notFound = true;
             int minChange = 0;
@@ -31,11 +32,11 @@ public class HalfTSPLocalSearch {
                 int vertex1Prev = cycle.get((index1 + verticesInCycleCount - 1) % verticesInCycleCount);
 
                 //wymiana łuków 2-opt
-                for (int index2 = index1 + 2; index2 < verticesInCycleCount && index2 != (index1 - 1) % verticesInCycleCount; ++index2) {
+                for (int index2 = index1 + 2; index2 < verticesInCycleCount && index2 != (index1 + verticesInCycleCount - 1) % verticesInCycleCount; ++index2) {
                     int vertex2 = cycle.get(index2);
                     int vertex2Next = cycle.get((index2 + 1) % verticesInCycleCount);
-                    int change = -adjacencyMatrix[vertex1][vertex1Next] - adjacencyMatrix[vertex2][vertex2Next] +
-                            adjacencyMatrix[vertex1][vertex2] + adjacencyMatrix[vertex1Next][vertex2Next];
+                    int change = adjacencyMatrix[vertex1][vertex2] + adjacencyMatrix[vertex1Next][vertex2Next] -
+                            adjacencyMatrix[vertex1][vertex1Next] - adjacencyMatrix[vertex2][vertex2Next];
                     if (change < minChange) {
                         minChange = change;
                         vertex1IndexToChange = index1;
@@ -46,8 +47,8 @@ public class HalfTSPLocalSearch {
 
                 //wymiana wierzchołków 2-opt
                 for (int vertex2 : freeVertices) {
-                    int change = -adjacencyMatrix[vertex1][vertex1Prev] - adjacencyMatrix[vertex1][vertex1Next] +
-                            adjacencyMatrix[vertex2][vertex1Prev] + adjacencyMatrix[vertex2][vertex1Next];
+                    int change = adjacencyMatrix[vertex2][vertex1Prev] + adjacencyMatrix[vertex2][vertex1Next] -
+                            adjacencyMatrix[vertex1][vertex1Prev] - adjacencyMatrix[vertex1][vertex1Next];
                     if (change < minChange) {
                         minChange = change;
                         vertex1IndexToChange = index1;
@@ -55,22 +56,24 @@ public class HalfTSPLocalSearch {
                         changeInsideCycle = false;
                     }
                 }
-
-                if (minChange < 0) {
-                    notFound = false;
-                    if (changeInsideCycle) {
-                        ArrayList<Integer> subPath = new ArrayList<>(cycle.subList(vertex1IndexToChange + 1, vertex2IndexToChange));
-                        cycle.removeAll(subPath);
-                        Collections.reverse(subPath);
-                        cycle.addAll(vertex1IndexToChange + 1, subPath);
-                    } else {
-                        cycle.remove(vertex1IndexToChange);
-                        cycle.add(vertex1IndexToChange, vertexToReplace);
-                        distance += minChange;
-                    }
-                }
             }
-        } while (notFound);
+
+            if (minChange < 0) {
+                notFound = false;
+                if (changeInsideCycle) {
+                    ArrayList<Integer> subPath = new ArrayList<>(cycle.subList(vertex1IndexToChange + 1, vertex2IndexToChange + 1));
+                    cycle.removeAll(subPath);
+                    Collections.reverse(subPath);
+                    cycle.addAll(vertex1IndexToChange + 1, subPath);
+                } else {
+                    freeVertices.add(cycle.get(vertex1IndexToChange));
+                    freeVertices.remove(new Integer(vertexToReplace));
+                    cycle.remove(vertex1IndexToChange);
+                    cycle.add(vertex1IndexToChange, vertexToReplace);
+                }
+                distance += minChange;
+            }
+        } while (!notFound);
         return new HalfTSPResult(distance, cycle);
     }
 }
